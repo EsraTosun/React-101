964d3f60c090fa242e4a93f20a9bd87e7c766267
EsraTosun <esratosun171@gmail.com>
2024-02-17 13:40:23 +0300

    Update Commit

---
+++ .history/1-Giris/callback_20240217132142.js
@@ -0,0 +1,39 @@
+// 1. ADIM
+// setTimeout(() => {
+//     console.log("Merhaba");
+// }, 2000) 
+// 2 saniye sonra fonksiyonun içindeki işlemi gerçekleştir.
+
+
+// 2. ADIM
+// setInterval(() =>{
+//     console.log("Merhaba ben her saniye çalışacağım");
+// }, 1000)
+// Durdurulmazsa 1 saniyde bir çalışacak.
+
+
+// 3. ADIM
+// const sayHi = (cb) => {
+//     cb();
+// };
+
+// sayHi(() => {
+//     console.log("Merhaba");
+// })
+
+
+// 4. ADIM
+import fetch from "node-fetch";
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((data) => data.json())  
+//API dan alınan data json formatından döndürülüyor
+.then((users) => {
+    console.log("Users Yüklendi!",users);
+
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((data) => data.json())  
+    .then((post) => console.log("Post Yüklendi!",post))
+});  
+//data users'a düşüyor ve oradan kullanmaya devam ediyoruz
+//Users işlemi bitttikten sonra postlar işlemine başla demek.
---
+++ .history/1-Giris/callback_20240217132902.js
@@ -0,0 +1,59 @@
+// 1. ADIM
+// setTimeout(() => {
+//     console.log("Merhaba");
+// }, 2000) 
+// 2 saniye sonra fonksiyonun içindeki işlemi gerçekleştir.
+
+
+// 2. ADIM
+// setInterval(() =>{
+//     console.log("Merhaba ben her saniye çalışacağım");
+// }, 1000)
+// Durdurulmazsa 1 saniyde bir çalışacak.
+
+
+// 3. ADIM
+// const sayHi = (cb) => {
+//     cb();
+// };
+
+// sayHi(() => {
+//     console.log("Merhaba");
+// })
+
+
+// 4. ADIM
+import fetch from "node-fetch";
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((data) => data.json())  
+// //API dan alınan data json formatından döndürülüyor
+// .then((users) => {
+//     console.log("Users Yüklendi!",users);
+
+//     fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((data) => data.json())  
+//     .then((post) => {
+//         console.log("Post1 Yüklendi!",post);
+
+//         fetch("https://jsonplaceholder.typicode.com/posts/2")
+//         .then((data) => data.json())  
+//         .then((post) => console.log("Post2 Yüklendi!",post));
+//     });
+// }); 
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+// .then((data) => data.json())  
+// .then((post) => console.log("Post Yüklendi!",post));
+//İkisi ayrı ayro yapılsaydı asektron işlem olduğu için aynı anda çalışacakti.
+//Hangisinin ilk olacağı belli olmayacaktı.
+
+//data users'a düşüyor ve oradan kullanmaya devam ediyoruz
+//Users işlemi bitttikten sonra postlar işlemine başla demek.
+
+async function getData() {
+    const data = fetch("https://jsonplaceholder.typicode.com/users");
+    console.log(data);
+}
+
+getData();
