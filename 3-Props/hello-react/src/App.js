684c13bf314c77607a6e123c5275aa21fcd03542
EsraTosun <esratosun171@gmail.com>
2024-02-18 19:48:51 +0300

    Update Commit

--- 3-Props/hello-react/src/App.js
+++ 3-Props/hello-react/src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 // Header=> html tagları ile karışmaması için büyük harfle başlanması gerekiyor.
 import User from './components/User';
 
+const friends=[
+  {
+    id: 1,
+    name: "Ahmet"
+  },
+  {
+    id: 2,
+    name: "Tayfun"
+  },
+  {
+    id: 3,
+    name: "Gökhan"
+  }
+];
 
 function App() {
   return (
     <div>
-      <User name="Esra" surname="Tosun" isLoggedIn = {true} age={29} /> 
+      <User 
+        name="Esra" 
+        surname="Tosun" 
+        isLoggedIn={true} 
+        age={29} 
+        friends={friends}
+      />
     </div>
   );
 }
--- 3-Props/hello-react/src/components/User.js
+++ 3-Props/hello-react/src/components/User.js
@@ -1,10 +1,40 @@
-function User({name ,surname, age, isLoggedIn}) {
+import PropTypes from "prop-types"
+
+function User({name ,surname, age, isLoggedIn,friends}) {
     console.log(isLoggedIn);
     return (
+       <>
        <h1>{
           isLoggedIn ? `${name} ${surname} ${age}` : "Giriş Yapmadınız!"
        }</h1>
+       {
+          friends.map((friend) => (
+            <div key={friend.id}>
+               {friend.id} - {friend.name}
+            </div>
+          ))
+       }
+       </>
+       
     );
 }
 
+//Gönderdiğimiz componentleri hangi veri tipi kabul ettiğimizi belirtmemiz gerekiyor
+User.PropTypes = {
+   name: PropTypes.string,
+   surname: PropTypes.string,
+   isLoggedIn: PropTypes.bool,
+   age: PropTypes.number,
+   friends: PropTypes.array
+}
+
 export default User;
+
+
+/* 
+   friends.map((friend , index) => (
+            <div key={index}>
+               {index} - {friend.name}
+            </div>
+          ))
+*/
