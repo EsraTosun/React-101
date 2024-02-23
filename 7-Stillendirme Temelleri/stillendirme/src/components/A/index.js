9fe9ae08ecdddc888d9ad2da5af3552c380d8235
EsraTosun <esratosun171@gmail.com>
2024-02-23 13:12:24 +0300

    Update Commit

---
+++ 7-Stillendirme Temelleri/stillendirme/src/components/A/index.js
@@ -0,0 +1,10 @@
+import styles from'./styles.module.css'
+const A = () => {
+  return (
+    <div className={styles.tittle}>
+        A
+    </div>
+  )
+}
+
+export default A
---
+++ 7-Stillendirme Temelleri/stillendirme/src/components/B/index.js
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+const B = () => {
+  return (
+    <div className = {styles.tittle}>
+        B
+    </div>
+  )
+}
+
+export default B
