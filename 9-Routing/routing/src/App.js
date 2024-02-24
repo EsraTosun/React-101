79baf45f5e532893dd70e6b953990b9542098b2d
EsraTosun <esratosun171@gmail.com>
2024-02-25 01:05:04 +0300

    Update Commit

--- 9-Routing/routing/src/App.js
+++ 9-Routing/routing/src/App.js
@@ -5,6 +5,9 @@ import Home from './pages/Home';
 import Users from './pages/Users';
 import Contact from './pages/Contact';
 import UserDetails from './pages/UserDetails';
+import Login from './pages/Login';
+import DashboardLayout from './layouts/DashboardLayout';
+import AuthLayout from './layouts/AuthLayout';
 
 import Menu from './components/Menu';
 
@@ -17,15 +20,21 @@ import {
   BrowserRouter
 } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
-      <Menu />
       <Routes>
-        <Route index element={<Home/>} />
-        <Route path='users' element={<Users />} />
-        <Route path='users/:id' element={<UserDetails />} />
-        <Route path='contact' element={<Contact />} />
+        <Route path='/' element = {<DashboardLayout />} >
+          <Route index element={<Home/>} />
+          <Route path='users' element={<Users />} />
+          <Route path='users/:id' element={<UserDetails />} />
+          <Route path='contact' element={<Contact />} />
+        </Route>
+        <Route path='auth' element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          {/* Auto girer girmez login saymasını görmek istediğimiz için index yazıldı */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
